f0330273a72417aa734aeceac6011cec7aa3b150
AvhiMaz <avhimazumder5@outlook.com>
2025-07-10 14:49:55 +0530

    add match func
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::io::stdout;
+use std::{
+    io::{Write, stdout},
+    time::{Duration, Instant},
+};
 
 use crossterm::{
-    cursor, execute,
+    cursor,
+    event::{self, Event, KeyCode},
+    execute,
     style::{self},
-    terminal::ClearType,
+    terminal::{ClearType, disable_raw_mode, enable_raw_mode},
 };
 use rand::seq::SliceRandom;
 
@@ -21,7 +26,7 @@ fn get_random_sentence() -> &'static str {
 }
 
 fn main() -> std::io::Result<()> {
-    let sentences = get_random_sentence();
+    let sentence = get_random_sentence();
     let mut stdout = stdout();
 
     execute!(
@@ -29,9 +34,51 @@ fn main() -> std::io::Result<()> {
         crossterm::terminal::Clear(ClearType::All),
         cursor::MoveTo(0, 0),
         style::Print("type the following:\n\n"),
-        style::Print(format!("{}\n\n", sentences)),
+        style::Print(format!("{}\n\n", sentence)),
         style::Print("Start typing:\n> ")
     )?;
+
+    enable_raw_mode()?; // raw mode, key presses are sent directly to the program without waiting for Enter.
+
+    let mut typed = String::new();
+    let mut started = false;
+    let mut started_time = Instant::now();
+
+    while typed != sentence {
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(key_event) = event::read()? {
+                if !started {
+                    started = true;
+                    started_time = Instant::now();
+                }
+
+                match key_event.code {
+                    KeyCode::Char(c) => {
+                        typed.push(c);
+                        print!("{}", c);
+                        stdout.flush()?;
+                    }
+                    KeyCode::Backspace => {
+                        if !typed.is_empty() {
+                            typed.pop();
+                            execute!(
+                                stdout,
+                                cursor::MoveLeft(1),
+                                style::Print(" "),
+                                cursor::MoveLeft(1)
+                            )?;
+                        }
+                    }
+                    KeyCode::Esc => {
+                        disable_raw_mode()?;
+                        println!("\n\nExited.");
+                        return Ok(());
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
 
     Ok(())
 }
