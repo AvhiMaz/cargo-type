97fab9120b5fdac5ef14458d606de4b6d9ea1f00
AvhiMaz <avhimazumder5@outlook.com>
2025-07-10 13:20:30 +0530

    random sentence
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::seq::SliceRandom;
+
+fn get_random_sentence() -> &'static str {
+    let sentences = [
+        "this is a random 1",
+        "this is a random 2",
+        "this is a random 3",
+        "this is a random 4",
+        "this is a random 5",
+    ];
+
+    let mut rng = rand::thread_rng();
+    sentences.choose(&mut rng).unwrap()
+}
+
 fn main() {
-    
+    let res = get_random_sentence();
+
+    println!("{}", res)
 }
